76cf534980fdfb129801b0ba297a9c9efee2414b
ResearcherNo1 <pawlov.andrusha2014@yandex.ru>
2020-04-03 23:37:38 +0400

    Updates

---
+++ precache-manifest.c8ded417cf93acdb437b77414ca959ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcadb964b6b40d63c70e1f3f989548e6",
+    "url": "/BookMaps/index.html"
+  },
+  {
+    "revision": "0c7c1165385371853888",
+    "url": "/BookMaps/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a3854df58a10a07dc753",
+    "url": "/BookMaps/static/js/2.b3abb0a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BookMaps/static/js/2.b3abb0a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c7c1165385371853888",
+    "url": "/BookMaps/static/js/main.5b160aa5.chunk.js"
+  },
+  {
+    "revision": "139b76bd40d666635ff4",
+    "url": "/BookMaps/static/js/runtime-main.66e8e4fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/BookMaps/static/media/logo.5d5d9eef.svg"
+  }
+]);
