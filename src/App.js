e304f0bb31fe7f5d6be2db6c81bc1a94409c4f38
ResearcherNo1 <pawlov.andrusha2014@yandex.ru>
2020-04-04 00:34:35 +0400

    Some test changes

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Хорошего времени суток тебе, пользователь!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://vk.com/although_fine"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Создатель
         </a>
       </header>
     </div>
