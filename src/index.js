056badbd32a46292467f2f1ccd083baf4e136409
ResearcherNo1 <pawlov.andrusha2014@yandex.ru>
2020-04-14 16:48:22 +0400

    Add VK UI dependencies & beta of interface

--- src/index.js
+++ src/index.js
@@ -1,17 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Epic, View, Panel, PanelHeader, Tabbar, TabbarItem } from '@vkontakte/vkui';
+import Icon28GlobeOutline from '@vkontakte/icons/dist/28/globe_outline';
+import Icon28ListOutline from '@vkontakte/icons/dist/28/list_outline';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import '@vkontakte/vkui/dist/vkui.css';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Example extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      activeStory: 'map'
+    };
+    this.onStoryChange = this.onStoryChange.bind(this);
+  }
+
+  onStoryChange (e) {
+    this.setState({ activeStory: e.currentTarget.dataset.story })
+  }
+
+  render () {
+    return (
+      <Epic activeStory={this.state.activeStory} tabbar={
+        <Tabbar>
+          <TabbarItem
+            onClick={this.onStoryChange}
+            selected={this.state.activeStory === 'feed'}
+            data-story="map"
+            text="Просмотр"
+          ><Icon28GlobeOutline/></TabbarItem>
+          <TabbarItem
+            onClick={this.onStoryChange}
+            selected={this.state.activeStory === 'chapter'}
+            data-story="chapter"
+            text="Главы"
+          ><Icon28ListOutline/></TabbarItem>
+        </Tabbar>
+      }>
+        <View id="map" activePanel="map">
+          <Panel id="map">
+            <PanelHeader>Карта</PanelHeader>
+          </Panel>
+        </View>
+        <View id="chapter" activePanel="chapter">
+          <Panel id="chapter">
+            <PanelHeader>Выбор главы</PanelHeader>
+          </Panel>
+        </View>
+      </Epic>
+    )
+  }
+}
+
+ReactDOM.render(<Example/>, document.getElementById('root'));
