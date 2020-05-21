fe1d2e67f6565b205030c8bd450b5d0faedb0772
ResearcherNo1 <pawlov.andrusha2014@yandex.ru>
2020-05-21 16:52:28 +0400

    Updates

---
+++ precache-manifest.34e3eb00913749b1603590f756e3b155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab138a42e92547cb9083949d73be1da",
+    "url": "/BookMaps/index.html"
+  },
+  {
+    "revision": "0531f0722b487194dc28",
+    "url": "/BookMaps/static/css/2.85b3d357.chunk.css"
+  },
+  {
+    "revision": "0531f0722b487194dc28",
+    "url": "/BookMaps/static/js/2.040475fb.chunk.js"
+  },
+  {
+    "revision": "2bd18aec346efbf72574573b7442e8aa",
+    "url": "/BookMaps/static/js/2.040475fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89170e9ecd3966825911",
+    "url": "/BookMaps/static/js/main.87a58012.chunk.js"
+  },
+  {
+    "revision": "05a4e50f7f959b9406d8",
+    "url": "/BookMaps/static/js/runtime-main.bd819692.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BookMaps/precache-manifest.f56e416efef4423c5dfa6abc0f6870ad.js"
+  "/BookMaps/precache-manifest.34e3eb00913749b1603590f756e3b155.js"
 );
 
 self.addEventListener('message', (event) => {
