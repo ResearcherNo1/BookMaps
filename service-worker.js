483391c19f4d89337aa30a90004829c245967439
ResearcherNo1 <pawlov.andrusha2014@yandex.ru>
2020-05-26 15:38:25 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BookMaps/precache-manifest.1e340f26d7f3a11ca0c784cb5f9125e1.js"
+  "/BookMaps/precache-manifest.384b88621b3a75765189232bcdcfbf94.js"
 );
 
 self.addEventListener('message', (event) => {
